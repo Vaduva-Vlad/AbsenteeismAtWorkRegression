efa826e72b4e33ff2a2348be6088af821c5220a6
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2024-06-22 15:33:11 +0300

    c

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ df_features=df.drop(['Absenteeism time in hours'], axis=1)
 df_target=pd.DataFrame(df['Absenteeism time in hours'])
 
 #Selectia atributelor
-dt=tree.DecisionTreeRegressor(max_depth=5,splitter='random', min_samples_leaf=3)
+dt=tree.DecisionTreeRegressor(max_depth=5,splitter='random', min_samples_leaf=2)
 attributes=[]
 for attribute in df_features.columns:
     df_X=pd.DataFrame(df_features[attribute])
@@ -35,7 +35,7 @@ for attribute in df_features.columns:
 
 
 m=0
-for i in range(100):
+for i in range(200):
     df_features=df_features[attributes]
     Xtrain, Xtest, ytrain, ytest = train_test_split(df_features, df_target, test_size=0.3, random_state=50)
     dt.fit(Xtrain,ytrain)
@@ -49,8 +49,10 @@ final_df = Xtest.copy()
 final_df["Y_original"] = ytest
 final_df["Y_predicted"] = ypred
 pass
-# Atribut: Reason for absence: 0.0807720741862058
+# Atribut: Reason for absence: 0.09280259457339013
 # Atribut: Day of the week: 0.023271586862661287
-# Atribut: Transportation expense: 0.035804070882330286
+# Atribut: Transportation expense: 0.038779640533143644
 # Atribut: Disciplinary failure: 0.010979521161990524
-# 0.23144537159780276
+# Atribut: Weight: 0.011392095480000974
+# Atribut: Body mass index: 0.014011062510043581
+# 0.2695110591750415
